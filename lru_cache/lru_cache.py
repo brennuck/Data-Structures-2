0908af7dfc87a6fec9fc34e63f1fb23205e76ee3
Brennon Nuckols <brennuck@gmail.com>
2020-05-02 12:26:26 -0600

    lru_cache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,5 @@
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +9,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.order = DoublyLinkedList() # Store the order
+        self.storage = dict() # Store key value pairs
+        self.size = 0 # Current size
+        self.limit = limit # Limit
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +22,12 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        if key in self.storage: # Checks if the key that is associated with the value is in storage
+            node = self.storage[key] # Pull the value of dict using the key
+            self.order.move_to_end(node) # Move the value to the end
+            return node.value[1] # update the position in the list
+        else: # If there isn't a key-value pair
+            return None # Return none
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +40,15 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        if key in self.storage: # if key is in storage
+            node = self.storage[key] # pull the value of dict using the key
+            node.value = (key, value) # assign values
+            self.order.move_to_end(node) # Move the value to the end
+            return # return
+        if self.size == self.limit: # if the storage is full
+            del self.storage[self.order.head.value[0]] # Delete the oldest entry
+            self.order.remove_from_head() # Remove the value from the list
+            self.size -= 1 #  - 1 from the storage
+        self.order.add_to_tail((key, value)) # Add the new pair to the dict
+        self.storage[key] = self.order.tail # The value assigned to the last item on the list (most recently added)
+        self.size += 1 # Adding a value to the size
